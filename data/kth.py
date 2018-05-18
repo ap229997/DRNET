b45c7ccb75a7f49171a0d261e32b45b915fac929
ap229997 <adityaprakash229997@gmail.com>
2018-05-18 02:03:23 +0000

    adding scripts for preprocessing different datasets

---
+++ data/kth.py
@@ -0,0 +1,92 @@
+import random
+import os
+import numpy as np
+import socket
+import torch
+from scipy import misc
+from torch.utils.serialization import load_lua
+
+######## Refer to the implementation given at 'https://github.com/edenton/drnet-py' for indepth understanding ###########
+
+class KTH(object):
+
+    def __init__(self, train, data_root, seq_len = 20, image_size=64, data_type='drnet'):
+        self.data_root = '%s/KTH/processed/' % data_root
+        self.seq_len = seq_len
+        self.data_type = data_type
+        self.image_size = image_size 
+        self.classes = ['boxing', 'handclapping', 'handwaving', 'jogging', 'running', 'walking']
+
+        self.dirs = os.listdir(self.data_root)
+        if train:
+            self.train = True
+            data_type = 'train'
+            self.persons = list(range(1, 21))
+        else:
+            self.train = False
+            self.persons = list(range(21, 26))
+            data_type = 'test'
+
+        self.data= {}
+        for c in self.classes:
+            self.data[c] = load_lua('%s/%s/%s_meta%dx%d.t7' % (self.data_root, c, data_type, image_size, image_size))
+     
+
+        self.seed_set = False
+
+    def get_sequence(self):
+        t = self.seq_len
+        while True: # skip seqeunces that are too short
+            c_idx = np.random.randint(len(self.classes))
+            c = self.classes[c_idx]
+            vid_idx = np.random.randint(len(self.data[c]))
+            vid = self.data[c][vid_idx]
+            seq_idx = np.random.randint(len(vid['files']))
+            if len(vid['files'][seq_idx]) - t >= 0:
+                break
+        dname = '%s/%s/%s' % (self.data_root, c, vid['vid'])
+        st = random.randint(0, len(vid['files'][seq_idx])-t)
+           
+
+        seq = [] 
+        for i in range(st, st+t):
+            fname = '%s/%s' % (dname, vid['files'][seq_idx][i])
+            im = misc.imread(fname)/255.
+            seq.append(im)
+        return np.array(seq)
+
+    # to speed up training of drnet, don't get a whole sequence when we only need 4 frames
+    # x_c1, x_c2, x_p1, x_p2 
+    def get_drnet_data(self):
+        c_idx = np.random.randint(len(self.classes))
+        c = self.classes[c_idx]
+        vid_idx = np.random.randint(len(self.data[c]))
+        vid = self.data[c][vid_idx]
+        seq_idx = np.random.randint(len(vid['files']))
+        dname = '%s/%s/%s' % (self.data_root, c, vid['vid'])
+        seq_len = len(vid['files'][seq_idx])
+           
+        seq = [] 
+        for i in range(4):
+            t = np.random.randint(seq_len)
+            fname = '%s/%s' % (dname, vid['files'][seq_idx][t])
+            im = misc.imread(fname)/255.
+            seq.append(im)
+        return np.array(seq)
+
+    def __getitem__(self, index):
+        if not self.seed_set:
+            self.seed_set = True
+            random.seed(index)
+            np.random.seed(index)
+            #torch.manual_seed(index)
+        if not self.train or self.data_type == 'sequence':
+            return torch.from_numpy(self.get_sequence())
+        elif self.data_type == 'drnet':
+            return torch.from_numpy(self.get_drnet_data())
+        else:
+            raise ValueError('Unknown data type: %d. Valid type: drnet | sequence.' % self.data_type)
+
+    def __len__(self):
+        return len(self.dirs)*36*5 # arbitrary
+
---
+++ data/suncg.py
@@ -0,0 +1,66 @@
+import random
+import os
+import numpy as np
+import socket
+import torch
+from scipy import misc
+
+######## Refer to the implementation given at 'https://github.com/edenton/drnet-py' for indepth understanding ###########
+
+def download_data(path):
+    full_path = '%s/suncg/single_object/chair/' % path
+    if not os.path.isdir(full_path):
+        print('Downloading SUNCG chair dataset to %s' % path)
+        import pdb; pdb.set_trace()
+        url = 'http://www.cs.nyu.edu/~denton/datasets/suncg_chairs.tar.gz'
+        os.system('wget -O %s/suncg_data.tar.gz %s' % (path, url))
+        os.system('tar -xzvf %s/suncg_data.tar.gz -C %s' % (path, path))
+    return full_path
+
+class SUNCG(object):
+
+  def __init__(self, train, data_root, seq_len = 20, image_size=64):
+    self.data_root = download_data(data_root)
+    self.seq_len = seq_len
+    self.image_size = image_size 
+
+    self.elevations = ['elevation_1.25', 'elevation_-1.00', 'elevation_0.50', 'elevation_2.00', 'elevation_-0.25']
+    self.dirs = os.listdir(self.data_root)
+    if train:
+      self.start_idx = 0
+      self.stop_idx = int(len(self.dirs)*0.85)
+      print('Loaded train data(%d - %d objects)' % (self.start_idx, self.stop_idx))
+    else:
+      self.start_idx = int(len(self.dirs)*0.85)+1 
+      self.stop_idx = len(self.dirs)
+      print('Loaded test data(%d - %d objects)' % (self.start_idx, self.stop_idx))
+ 
+
+    self.seed_set = False
+
+  def get_sequence(self):
+    t = self.seq_len
+    idx = np.random.randint(self.start_idx, self.stop_idx)
+    obj_dir = self.dirs[idx]
+    elevation = self.elevations[random.randint(0, len(self.elevations)-1)]
+    dname = '%s/%s/%s' % (self.data_root, obj_dir, elevation)
+    
+    st = random.randint(1, 36-t)
+    seq = [] 
+    for i in range(st, st+t):
+      fname = '%s/p200-64_%d.png' % (dname, i)
+      im = misc.imread(fname)/255.
+      seq.append(im)
+    return np.array(seq)
+
+  def __getitem__(self, index):
+    if not self.seed_set:
+      self.seed_set = True
+      random.seed(index)
+      np.random.seed(index)
+      #torch.manual_seed(index)
+    return torch.from_numpy(self.get_sequence())
+
+  def __len__(self):
+    return len(self.dirs)*36*5
+
